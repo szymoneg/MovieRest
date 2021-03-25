f552d69c388b20000a831d212848f71457e30633
szymoneg <szymon.zsp5@gmail.com>
2021-03-25 21:15:10 +0100

    add apiservice

--- FetchMovieList/ApiService.swift
+++ FetchMovieList/ApiService.swift
@@ -10,7 +10,44 @@ import Foundation
 class ApiService {
     private var dataTask: URLSessionDataTask?
     
-    func getPopularMoviesData(completion: (Result<MoviesData, Error>) -> Void){
+    func getPopularMoviesData(completion: @escaping (Result<MoviesData, Error>) -> Void){
+        let moviesURL = "https://api.themoviedb.org/3/movie/popular?api_key=08d9aa3c631fbf207d23d4be591ccfc3&page=1"
         
+        guard let url = URL(string: moviesURL) else {
+            return
+        }
+        
+        dataTask = URLSession.shared.dataTask(with: url){ (data, response, error) in
+            if let error = error{
+                completion(.failure(error))
+                print("Datatask error")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else{
+                print("Empty response")
+                return
+            }
+            
+            print("Response status code \(response.statusCode)")
+            
+            guard let data = data else{
+                print("Empty Data")
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(MoviesData.self, from: data)
+                
+                DispatchQueue.main.async {
+                    completion(.success(jsonData))
+                }
+                
+            }catch let error{
+                completion(.failure(error))
+            }
+        }
+        dataTask?.resume()
     }
 }
--- FetchMovieList/ViewController.swift
+++ FetchMovieList/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var apiService = ApiService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        apiService.getPopularMoviesData{ (result) in
+            print(result)
+        }
     }
 
 
